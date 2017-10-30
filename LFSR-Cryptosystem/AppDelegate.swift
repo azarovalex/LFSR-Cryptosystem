dd4740fbc00d735b1feeaadc7c681b43af6d2b3b
azarovalex <alex@azarov.by>
2017-10-30 21:57:11 +0300

    Initial Commit

---
+++ LFSR-Cryptosystem/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LFSR-Cryptosystem
+//
+//  Created by Alex Azarov on 30/10/2017.
+//  Copyright © 2017 Alex Azarov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
